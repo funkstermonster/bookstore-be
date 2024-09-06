b2a5297f137c040ae92fe281d34784be9fc684d2
funkstermonster <fanni.szasz96@gmail.com>
2024-09-06 17:59:59 +0200

    create laravel be project, add db connection infos, create migration for books table

---
+++ database/migrations/2024_09_06_000001_init_db.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('author');
+            $table->string('title');
+            $table->date('publish_date');
+            $table->string('isbn', 50)->unique();
+            $table->text('summary');
+            $table->decimal('price', 8, 2);
+            $table->integer('on_store');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
