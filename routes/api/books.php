057ecf3cf74191317faa2e3e6ef6b675e3f055c1
funkstermonster <fanni.szasz96@gmail.com>
2024-09-12 16:21:23 +0200

    remove unnecessary code

--- routes/api/books.php
+++ routes/api/books.php
@@ -4,15 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\BookController;
 
-// Route::get('/books', [BookController::class, 'index']);
-
-// Route::get('/books/{book}', [BookController::class, 'show']);
-
-// Route::post('/books', [BookController::class, 'store']);
-
-// Route::put('/books/{book}', [BookController::class, 'update']);
-
-// Route::delete('/books/{book}', [BookController::class, 'destroy']);
-
 Route::apiResource('books', BookController::class);
 
